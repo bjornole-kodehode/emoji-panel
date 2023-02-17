8bedab97fb337c6ac0979e7491ed26f1e0791fef
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-02-17 16:56:02 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import EmojiPanel from './components/EmojiPanel'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <EmojiPanel />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/EmojiPanel.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from 'react'
+interface Emoji {
+        character: string,
+        unicodeName: string,
+        codePoint: string,
+        group: number;
+        subGroup: number;
+    }
+const EmojiPanel = () => {
+    const [emojis, setEmojis] = useState<Emoji[]>([])
+    const [displayEmojisPanel, setDisplayEmojisPanel] = useState(false)
+    
+    const emojiPanelRef = useRef(null)
+    useEffect(() => {
+        const onClick = (e: any) => {
+            if (e.target !== emojiPanelRef.current) {
+                setDisplayEmojisPanel(false)
+            }
+        }
+    
+        async function fetchEmojis() {
+            await fetch("https://emoji-api.com/emojis?access_key=52b74a2dd02e74464659c38b6fe86c12c5a8d74e")
+                .then(res => res.json())
+                .then(data => setEmojis(data.map((emoji: any) => emoji)))
+        }
+        fetchEmojis()
+        document.addEventListener("click", onClick)
+        return () => document.removeEventListener("click", onClick)
+    
+    }, [])
+    const random = Math.floor(Math.random() * 2693)
+    const randomEmoji = emojis[random]?.character
+    return (
+    <div ref={emojiPanelRef} onClick={(e) => e.stopPropagation()}>
+        <div className='emoji-panel-container'>
+            <input />
+          
+            <div className='emoji-panel'>
+                <button onClick={() => setDisplayEmojisPanel(prevState => !prevState)}>{randomEmoji}</button>
+                <div className="emojis">
+                    {displayEmojisPanel && emojis.map(((emoji: any, idx) => <div className='emoji' key={idx}>{emoji?.character}</div>))}
+                </div>
+            </div>
+
+        </div>
+    </div>
+    )
+  
+}
+
+export default EmojiPanel
